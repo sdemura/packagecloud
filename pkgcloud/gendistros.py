71104476569e53894d57d2aae76564a94304849b
Sean Demura <sean.demura@corelight.com>
2023-12-05 09:35:15 -0500

    update distros

--- pkgcloud/gendistros.py
+++ pkgcloud/gendistros.py
@@ -1,34 +1,35 @@
-#!/usr/bin/env python
-# Generate Go map of distros supported by Packagecloud API
-# By generating the list once, we save an expensive API call.
-# See https://packagecloud.io/docs/api#resource_distributions
+#!/usr/bin/env python3
 
-import os, sys
-import urllib
 import json
+import os
 
-var = sys.argv[1]
-token = os.environ['PACKAGECLOUD_TOKEN']
+import requests
 
-url = 'https://%s:@packagecloud.io/api/v1/distributions.json' % token
-resp = urllib.urlopen(url)
-data = json.loads(resp.read())
+PACKAGECLOUD_TOKEN = os.environ.get("PACKAGECLOUD_TOKEN")
 
-result = {}
-for distros in data.values():
-    for d in distros:
-        for v in d['versions']:
-            k = d['index_name']
-            if 'index_name' in v:
-                k = '/'.join([k, v['index_name']])
-            v = v['id']
-            result[k] = v
+r = requests.get(
+    f"https://{PACKAGECLOUD_TOKEN}:@packagecloud.io/api/v1/distributions.json"
+)
+r.raise_for_status()
 
-print '// Generated with %s' % __file__
-print
-print 'package pkgcloud'
-print
-print 'var %s = map[string]int{' % var
-for k, v in sorted(result.items(),key=lambda x:x[1]):
-    print "\t\"%s\": %d," % (k, v)
-print '}'
+j = r.json()
+
+print("// Generated with .gendistro.py")
+print("package pkgcloud")
+print("var supportedDistros = map[string]int{")
+try:
+    for k in j.keys():
+        package_type = j[k]
+
+        for distros in package_type:
+            distro_name = distros["index_name"]
+            versions = distros["versions"]
+
+            for version in versions:
+                v = version["index_name"]
+                id = version["id"]
+                distro = f'"{distro_name}/{v}": {id},'
+                print(distro)
+except:
+    pass
+print("}")
